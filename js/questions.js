3458a1d7ef52ae8ade57afafbcb94d127cb9181b
Demo <98008095+nunnapatphuttha@users.noreply.github.com>
2022-06-15 13:15:43 +0700

    first commit

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "lorem?",
+    answer: "O1",
+    options: [
+      "O1",
+      "O2",
+      "O3",
+      "O4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "lorem?",
+    answer: "O1",
+    options: [
+      "O1",
+      "O2",
+      "O3",
+      "O4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "lorem?",
+    answer: "O1",
+    options: [
+      "O1",
+      "O2",
+      "O3",
+      "O4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "lorem?",
+    answer: "O1",
+    options: [
+      "O1",
+      "O2",
+      "O3",
+      "O4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+   numb: 7,
+   question: "What does XML stand for?",
+   answer: "eXtensible Markup Language",
+   options: [
+     "eXtensible Markup Language",
+     "eXecutable Multiple Language",
+     "eXTra Multi-Program Language",
+     "eXamine Multiple Language"
+   ]
+ },
+ {
+  numb: 8,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+{
+ numb: 9,
+ question: "What does XML stand for?",
+ answer: "eXtensible Markup Language",
+ options: [
+   "eXtensible Markup Language",
+   "eXecutable Multiple Language",
+   "eXTra Multi-Program Language",
+   "eXamine Multiple Language"
+ ]
+},
+{
+ numb: 10,
+ question: "What does XML stand for?",
+ answer: "eXtensible Markup Language",
+ options: [
+   "eXtensible Markup Language",
+   "eXecutable Multiple Language",
+   "eXTra Multi-Program Language",
+   "eXamine Multiple Language"
+ ]
+}
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
